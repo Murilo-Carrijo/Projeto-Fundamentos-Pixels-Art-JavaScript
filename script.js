61fd5329c140a69cde5389ed769f20f49acc8132
murilo <murilocarrijoadm@gmail.com>
2022-10-24 22:13:17 -0300

    refator: requisito 2 - criar a paleta de cores

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const colorPalette = document.getElementsByClassName('color');
 const pixel = document.getElementsByClassName('pixel');
 const clear = document.getElementById('clear-board');
+const colors = ['black', 'red', 'blue', 'green'];
 
 function createTitle() {
   const header = document.querySelector('.header-container');
@@ -10,8 +11,22 @@ function createTitle() {
   header.appendChild(title);
 }
 
+function createPaletteColor(arrColors) {
+  const colorsContainer = document.getElementById('color-palette');
+  for (let index = 0; index < arrColors.length; index += 1) {
+    const buttonColor = document.createElement('button');
+    buttonColor.type = 'button';
+    buttonColor.id = arrColors[index];
+    buttonColor.className = 'color';
+    colorsContainer.appendChild(buttonColor);
+  }
+  const firstElement = document.querySelector('.color');
+  firstElement.className = 'color selected';
+}
+
 function receiveClick(color) {
   for (let index = 0; index < colorPalette.length; index += 1) {
+    console.log('olá');
     if (colorPalette[index].classList.contains('selected')) {
       colorPalette[index].classList.toggle('selected');
     }
@@ -47,6 +62,7 @@ function clearColorPalette() {
 
 clear.addEventListener('click', clearColorPalette);
 
-window.onload = function () {
+window.onload = () => {
   createTitle();
+  createPaletteColor(colors);
 };
